d807289cc66c22653d4f44b984efc5652cae7277
jules white <jules.white1@protonmail.com>
2024-11-07 22:01:57 +0100

    Update stardust.zig to use a new allocator system

--- src/stardust.zig
+++ src/stardust.zig
@@ -1,28 +1,5 @@
 const std = @import("std");
 
-var gpa: ?std.heap.GeneralPurposeAllocator(.{}) = null;
-var alloc: std.mem.Allocator = undefined;
-var GLOBAL_LOG_LEVEL: sd_log_level = .debug;
-
-pub fn sd_init_log(log_level: ?sd_log_level, allocator: ?std.mem.Allocator) !void {
-    if (allocator) |a| {
-        alloc = a;
-    } else {
-        gpa = std.heap.GeneralPurposeAllocator(.{}){};
-        alloc = gpa.?.allocator();
-    }
-    if(log_level) |l| {
-        GLOBAL_LOG_LEVEL = l;
-    } else {
-        GLOBAL_LOG_LEVEL = .debug;
-    }
-}
-
-pub fn sd_deinit_log() void {
-    const deinit_status = gpa.?.deinit();
-    if (deinit_status == .leak) std.debug.panic("Could not properly deinit general purpose allocator", .{});
-}
-
 pub const sd_log_level = enum(u8) {
     info = 0,
     debug = 1,
@@ -40,21 +17,49 @@ pub const sd_log_level = enum(u8) {
 
     pub fn colour(self: *const sd_log_level) []const u8 {
         return switch (self.*) {
-            .info => "\x1b[94m",
             .debug => "\x1b[38;5;91m",
+            .info => "\x1b[94m",
             .err => "\x1b[96m",
             .fatal => "\x1b[38;5;1m",
         };
     }
 };
 
-pub fn sdlog(source: std.builtin.SourceLocation, args: anytype) void {
-    var string = std.ArrayList(u8).init(alloc);
+// Global config of Stardust
+var SD_CONFIG = _sd_global_config{ .alloc = null, .level = .info };
+const _sd_global_config = struct {
+    alloc: ?std.mem.Allocator,
+    level: sd_log_level,
+};
+
+// Setup Stardust allocator and other configuration
+pub fn sd_setup(args: struct {
+    alloc: std.mem.Allocator,
+    level: ?sd_log_level,
+}) void {
+    if (args.level) |l| {
+        SD_CONFIG.level = l;
+    } else {
+        SD_CONFIG.level = switch (std.builtin.OptimizeMode) {
+            .Debug => sd_log_level.debug,
+            .ReleaseSafe => sd_log_level.info,
+            else => sd_log_level.err,
+        };
+    }
+    SD_CONFIG.alloc = args.alloc;
+}
+
+pub fn log(args: anytype) void {
+    if(SD_CONFIG.alloc == null) {
+        std.debug.print("Stardust has not been initialised. Please call sd_setup()");
+    }
+    var string = std.ArrayList(u8).init(SD_CONFIG.alloc.?);
     defer string.deinit();
 
     const args_type = @TypeOf(args);
 
     var level: sd_log_level = .debug;
+    var source: std.builtin.SourceLocation = undefined;
 
     inline for (@typeInfo(args_type).Struct.fields) |field| {
         const field_value = @field(args, field.name);
@@ -67,7 +72,7 @@ pub fn sdlog(source: std.builtin.SourceLocation, args: anytype) void {
                 std.debug.panic("Could not append slice: {}", .{e});
             };
         } else if (isZigInt(field_type)) {
-            var buf: [20]u8 = undefined;
+            var buf: [32]u8 = undefined;
             _ = std.fmt.bufPrint(&buf, "{}", .{field_value}) catch "";
             string.appendSlice(&buf) catch {};
             string.appendSlice(" ") catch {};
@@ -88,7 +93,7 @@ pub fn sdlog(source: std.builtin.SourceLocation, args: anytype) void {
 
     const final_string = string.toOwnedSlice() catch "";
 
-    if (@intFromEnum(level) >= @intFromEnum(GLOBAL_LOG_LEVEL)) {
+    if (@intFromEnum(level) >= @intFromEnum(SD_CONFIG.level)) {
         std.debug.print("\x1b[38;5;13m{s}\x1b[38;5;255m:\x1b[33m{s}\x1b[38;5;255m:\x1b[38;5;63m{}\x1b[38;5;255m \x1b[38;5;255m:: {s}\x1b[3m{s}\x1b[38;5;255m\x1b[0m :: {s}\n", .{
             source.file, source.fn_name, source.line, level.colour(), level.to_string(), final_string,
         });
