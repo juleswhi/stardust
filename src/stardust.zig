40d8c7c6cff8a74c56d8ce32874fc5316ee2bcde
jules white <jules.white1@protonmail.com>
2024-11-12 01:54:17 +0100

    proper slice capturing

--- src/stardust.zig
+++ src/stardust.zig
@@ -153,8 +153,7 @@ fn _sd_print(msg: log_message) void {
 
     if (msg.description) |d| {
         for (d) |line| {
-            std.debug.print("concat {s}\n", .{line});
-            _ = std.mem.concat(SD_CONFIG.alloc.?, u8, &[4][]const u8{ desc.?, "\n", "  |> ", line }) catch {
+            desc = std.mem.concat(SD_CONFIG.alloc.?, u8, &[4][]const u8{ desc.?, "\n", "  ", line }) catch {
                 std.debug.print("Could not properly concat memory", .{});
             };
         }
