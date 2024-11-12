175818c96936f53463fb3be2f52a63c084352a01
jules white <jules.white1@protonmail.com>
2024-11-12 01:34:16 +0100

    return return

--- src/stardust.zig
+++ src/stardust.zig
@@ -151,7 +151,6 @@ fn _sd_print(msg: log_message) void {
         for (d) |line| {
             _ = std.mem.concat(SD_CONFIG.alloc.?, u8, &[4][]const u8{ desc.?, "\n", "  |> ", line }) catch {
                 std.debug.print("Could not properly concat memory", .{});
-                return "";
             };
         }
     } else {
